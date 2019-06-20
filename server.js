b208a6f2fe57cf6c09fe586392dc474211377354
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-20 21:11:20 +0000

    add header and footer in order page

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.use(express.static("public"));
 //app.use("/api/users", usersRoutes(knex));
 
 // Home page
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/restaurents/order", (req, res) => {
+  res.render("order");
 });
 
 
