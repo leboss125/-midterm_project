8d493cb2c35e9134a9c1ea41f04e692ce900e072
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-21 00:10:01 +0000

    Add info required to use twiollio and npm install twiollio

--- routes/order_route.js
+++ routes/order_route.js
@@ -10,6 +10,20 @@ module.exports = (knex) => {
   });
 
   router.post("/:id", (req, res) => {
+
+    //to send message to customer that order is ready to pick up
+    var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; // Your Account SID from www.twilio.com/console
+    var authToken = 'your_auth_token';   // Your Auth Token from www.twilio.com/console
+
+    var twilio = require('twilio');
+    var client = new twilio(accountSid, authToken);
+
+    client.messages.create({
+        body: 'Your order is ready',
+        to: '+16137203194',  // Text this number
+        from: '+12345678901' // From a valid Twilio number
+})
+.then((message) => console.log(message.sid));
     res.redirect("/");
   });
 
