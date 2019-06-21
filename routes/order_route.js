9fff8156b7286207df15289949435f099f8aa68c
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-21 17:57:05 +0000

    Merging feature/order-routes

---
+++ routes/order_route.js
@@ -0,0 +1,32 @@
+  "use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+  router.get("/:id", (req, res) => {
+    res.render("order");
+  });
+
+  router.post("/:id", (req, res) => {
+
+    //to send message to customer that order is ready to pick up
+    var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; // Your Account SID from www.twilio.com/console
+    var authToken = 'your_auth_token';   // Your Auth Token from www.twilio.com/console
+
+    var twilio = require('twilio');
+    var client = new twilio(accountSid, authToken);
+
+    client.messages.create({
+        body: 'Your order is ready',
+        to: '+16137203194',  // Text this number
+        from: '+12345678901' // From a valid Twilio number
+})
+.then((message) => console.log(message.sid));
+    res.redirect("/");
+  });
+
+
+  return router;
+}
