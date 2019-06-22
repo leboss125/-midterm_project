b90b7d081eec6b14a1d1a40006ae76caf4a07d80
Barry <barry2014158@gmail.com>
2019-06-22 00:28:48 +0000

    merging data branch with master

--- routes/restaurants_route.js
+++ routes/restaurants_route.js
@@ -3,10 +3,27 @@
 const express = require('express');
 const router  = express.Router();
 
+
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-    res.render('restaurants');
+    //console.log(res.param,res.body);
+    //console.log(req.param;
+    //console.log(req.body);
+
+    let templateVars = {};
+
+    (knex.select("username")
+        .from("customers")
+        .where('username','mclovin69@gmail.com').then(value => {
+
+          templateVars['restoName'] = value;
+          console.log(templateVars);
+          res.render('restaurants',templateVars);
+
+        })).finally()
+
+
   });
 
   return router;
