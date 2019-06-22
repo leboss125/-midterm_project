e5b1d6e39885c3b081f9ea825343e6deeb67dbac
Barry <barry2014158@gmail.com>
2019-06-22 23:40:33 +0000

    testing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,37 @@
 //     }
 //   });;
 // });
+
+
+
+var order = {
+
+}
+
+
+$(document).ready(function() {
+
+  console.log();
+  document.getElementById('add').addEventListener("click", (event) => {
+    var totalCount = $(".counter").text();
+  console.log(totalCount);
+    totalCount++;
+    $(".counter").text(totalCount);
+  });
+
+  document.getElementById('remove').addEventListener("click", (event) => {
+    totalCount--;
+    $(".counter").text(totalCount);
+  });
+
+
+});
+
+/*
+var totalCount;
+
+document.getElementById('add-tweet').children[0].addEventListener("input", (event) => {
+    var totalCount = 140 - event.target.value.length;
+    $(".counter").text(totalCount);
+  });
+*/
--- routes/order_route.js
+++ routes/order_route.js
@@ -4,24 +4,26 @@ const express = require('express');
 const router  = express.Router();
 var twilio = require('twilio');
 
+
+
 module.exports = (knex) => {
 
-  router.get("/:id", (req, res) => {  
+  router.get("/:id", (req, res) => {
     //obtain data from database
      knex
       .select('restaurents.id','restaurents.name', 'menu_items.name','restaurent_id','price')
       .from("menu_items")
       .join('restaurents', {'restaurents.id':'menu_items.restaurent_id'})
-      .where('restaurent_id','=',req.params.id) //instead of 1 -> use req.param to get id 
+      .where('restaurent_id','=',req.params.id) //instead of 1 -> use req.param to get id
       .then((results) => {
 
         res.render("order",{menus:results});
-        console.log(results[0].name); 
-        console.log(results); 
-        
-    }); 
+        console.log(results[0].name);
+        console.log(results);
+
+    });
     //.finally(() => knex.destroy());
- 
+
   });
 
   router.post("/:id", (req, res) => {
@@ -29,7 +31,7 @@ module.exports = (knex) => {
     var accountSid = 'AC324e4b947a9a446ad2250037f1e9f268'; // Your Account SID from www.twilio.com/console
     var authToken = 'cc2f2227e77ac2b565ddab161df5b77c';   // Your Auth Token from www.twilio.com/console
 
-    
+
     var client = new twilio(accountSid, authToken);
 
     client.messages.create({
