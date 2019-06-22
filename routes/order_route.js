cabb319a6253268e0489d00b0f0cf90605a4ac7c
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-22 16:56:13 +0000

    Remove finally knex destroy

--- routes/order_route.js
+++ routes/order_route.js
@@ -2,26 +2,37 @@
 
 const express = require('express');
 const router  = express.Router();
+var twilio = require('twilio');
 
 module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
-    res.render("order");
+
+    //obtain data from database
+     knex
+      .select("name")
+      .from("menu_items")
+      .then((results) => {
+        console.log(results);
+        res.render("order",results);
+        
+    }); 
+    //.finally(() => knex.destroy());
+ 
   });
 
   router.post("/:id", (req, res) => {
-
     //to send message to customer that order is ready to pick up
-    var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; // Your Account SID from www.twilio.com/console
-    var authToken = 'your_auth_token';   // Your Auth Token from www.twilio.com/console
+    var accountSid = 'AC324e4b947a9a446ad2250037f1e9f268'; // Your Account SID from www.twilio.com/console
+    var authToken = 'cc2f2227e77ac2b565ddab161df5b77c';   // Your Auth Token from www.twilio.com/console
 
-    var twilio = require('twilio');
+    
     var client = new twilio(accountSid, authToken);
 
     client.messages.create({
         body: 'Your order is ready',
         to: '+16137203194',  // Text this number
-        from: '+12345678901' // From a valid Twilio number
+        from: '+12897961008' // From a valid Twilio number
 })
 .then((message) => console.log(message.sid));
     res.redirect("/");
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 const cookieSession = require('cookie-session');
+var twilio = require('twilio');
 
 
 // Seperated Routes for each Resource
