863269bf350111cd18d3c97d0b82c85accb2a1ae
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-23 12:46:44 +0000

    Decrease image of restaurents when hover over them

--- db/seeds/restaurants_seed.js
+++ db/seeds/restaurants_seed.js
@@ -6,13 +6,13 @@ exports.seed = function(knex, Promise) {
         return Promise.all([
           // Inserts seed entries
           knex('restaurents').insert({name: "MANDY'S", phone_number:5146707820, address:'201 LAURIER OUEST', image:'https://images.deliveryhero.io/image/fd-ca/LH/e3ng-listing.jpg?width=800&height=584'}),
-          knex('restaurents').insert({name: "CHEF ON CALL", phone_number:5148442044, address:'3430 PARC AVENUE'}),
-          knex('restaurents').insert({name: "LE BAY CÀ PHÊ", phone_number:5145501283, address:'5263 BOULEVARD SAINT-LAURENT'}),
-          knex('restaurents').insert({name: "BURGER DE VILLE", phone_number:5145075050, address:'5282 BOULEVARD SAINT-LAURENT'}),
-          knex('restaurents').insert({name: "CARIBOU GOURMAND", phone_number:4383876677, address:'5308 BOULEVARD ST LAURENT'}),
-          knex('restaurents').insert({name: "PIZZERIA MAGPIE", phone_number:5145072900, address:'16 RUE MAGUIRE'}),
-          knex('restaurents').insert({name: "BOB POULET NINJA", phone_number:5145075050, address:'5282 BOULEVARD ST LAURENT'}),
-          knex('restaurents').insert({name: "FRANCIS'S CHICKEN", phone_number:5148449999, address:'5455 Gaspe Avenue'}),
+          knex('restaurents').insert({name: "CHEF ON CALL", phone_number:5148442044, address:'3430 PARC AVENUE', image:'https://images.deliveryhero.io/image/fd-ca/LH/s7ey-listing.jpg?width=800&height=584'}),
+          knex('restaurents').insert({name: "LE BAY CÀ PHÊ", phone_number:5145501283, address:'5263 BOULEVARD SAINT-LAURENT', image:'https://images.deliveryhero.io/image/fd-ca/LH/s0oy-listing.jpg?width=800&height=584'}),
+          knex('restaurents').insert({name: "BURGER DE VILLE", phone_number:5145075050, address:'5282 BOULEVARD SAINT-LAURENT', image:'https://images.deliveryhero.io/image/fd-ca/LH/s1nq-listing.jpg?width=800&height=584'}),
+          knex('restaurents').insert({name: "CARIBOU GOURMAND", phone_number:4383876677, address:'5308 BOULEVARD ST LAURENT', image:'https://images.deliveryhero.io/image/fd-ca/LH/s6gu-listing.jpg?width=800&height=584'}),
+          knex('restaurents').insert({name: "PIZZERIA MAGPIE", phone_number:5145072900, address:'16 RUE MAGUIRE', image:'https://images.deliveryhero.io/image/fd-ca/LH/s3xg-listing.jpg?width=800&height=584'}),
+          knex('restaurents').insert({name: "BOB POULET NINJA", phone_number:5145075050, address:'5282 BOULEVARD ST LAURENT', image:'https://images.deliveryhero.io/image/fd-ca/LH/s3fv-listing.jpg?width=800&height=584'}),
+          knex('restaurents').insert({name: "FRANCIS'S CHICKEN", phone_number:5148449999, address:'5455 Gaspe Avenue', image:'https://images.deliveryhero.io/image/fd-ca/LH/s3fv-listing.jpg?width=800&height=584'}),
         ]);
       });
   };
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,9 +16,7 @@ console.log("poulet");
 
 $(document).ready(function() {
 
-  var count = {
-
-  }
+  var counterObject = {}; 
   var counter = 0;
   //var ids = new Array();
   $('button[id]').each(function() { //Get elements that have an id=
@@ -26,14 +24,19 @@ $(document).ready(function() {
    $(this).click(function(){
 
     if(this.getAttribute('id').includes('add')===true){
-      let arg = parseInt(this.getAttribute("id").split(":")[1]);
-      //console.log(arg)
+      var arg = parseInt(this.getAttribute("id").split(":")[1]);
+      console.log('hello'); 
+      console.log(arg); 
+      counterObject[arg]=0; 
       counter++;
       if(counter>=5){
         counter = 5;
       }
     }
     else {
+        var arg = parseInt(this.getAttribute("id").split(":")[1]);
+        console.log('bye'); 
+        console.log(arg); 
       counter--;
       if(counter<0){
         counter = 0;
@@ -44,5 +47,5 @@ $(document).ready(function() {
    //ids.push($(this).attr(“id”)); //add id to array
   });
 
-
+console.log(counterObject); 
 });
