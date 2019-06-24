fa780841d13a9396c632e137bdb2fe04ccc8da5c
Barry <barry2014158@gmail.com>
2019-06-24 03:34:24 +0000

    send data from frontend to server

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,12 +14,11 @@ $(document).ready(function() {
   }
   console.log('results: ',orderSummary);
 
-
   function checkFrequency(){
 
     var obj = {};
     for(let item in orderSummary){
-      console.log(item, orderSummary[item]);
+      //console.log(item, orderSummary[item]);
       if (orderSummary[item]['freq'] != 0){
         obj[item] = orderSummary[item];
       }
@@ -35,51 +34,89 @@ $(document).ready(function() {
       html +=`<div>
                 <span>${orderObj[item].name}</span>
                 <span>Quantity: ${orderObj[item].freq}</span>
-                <span>$${round(orderObj[item].price * orderObj[item].freq)}</span>
+                <span>$${(orderObj[item].price * orderObj[item].freq).toFixed(2)}</span>
               </div>`;
       total += orderObj[item].price * orderObj[item].freq;
     }
     html += `<div>
-                <span> BEFORE TAX: $${round(total)} </span>
-                <span> TAX: $${round(total*0.15)} </span>
-                <span> TOTAL: $${round(total*1.15)} </span>
+                <span> BEFORE TAX: $${total.toFixed(2)} </span>
+                <span> TAX: $${(total * 0.15).toFixed(2)} </span>
+                <span > TOTAL: $<b id="total">${(total * 1.15).toFixed(2)}</b> </span>
               </div>`
 
     return html;
   };
-
-  function round(number) {
-    return Math.round(number * 100) / 100;
-  }
-
-
 
   $('button[id]').each(function() { //Get elements that have an id=
 
    $(this).click(function(){
 
     var index = parseInt(this.getAttribute("id").split(":")[1]);
-    if(this.getAttribute('id').includes('add')===true){
+    if(this.getAttribute('id').includes('add') === true){
 
       orderSummary[index]['freq'] ++;
 
-      if(orderSummary[index]['freq'] >=5){
+      if(orderSummary[index]['freq'] >= 5){
         orderSummary[index]['freq'] = 5;
       }
     }
-    else if (this.getAttribute('id').includes('remove')===true){
+    else if (this.getAttribute('id').includes('remove') === true){
+
       orderSummary[index]['freq'] --;
 
       if(orderSummary[index]['freq'] < 0){
         orderSummary[index]['freq'] = 0;
       }
     }
-    console.log(orderSummary);
 
     $("#result span").remove()
     $("#result").prepend((createOrder(checkFrequency())));
+
+
+
+    /*$.ajax({
+      method: 'post',
+      url: '/restaurant',
+      data: {
+        items: checkFrequency(),
+        total_price: parseFloat($("#total").text())
+      }
+
+    }).done(result => console.log(result)) */
+
+
     });
 
   });
+
+  $("#order-now").on('click',function(){
+
+
+
+      var finalTotal = 0
+      var temp = checkFrequency();
+
+      for(item in temp){
+        finalTotal += temp[item].price * temp[item].freq;
+      }
+
+      var restoId = window.location.pathname.split('/')[window.location.pathname.split('/').length-1]
+
+      console.log('testing result: ' ,checkFrequency(), finalTotal, restoId);
+
+        $.ajax({
+          method: 'post',
+          url: '/restaurant',
+          data: {
+            items: checkFrequency(),
+            total_price: finalTotal,
+            restaurant_id: restoId
+          }
+        }).done(result => console.log(result))
+
+    });
+
+  //POST REQUEST FOR ORDER STATUS TABLE: id, customer_id, restaurant_id, items(object), total_price
+  // created_at, status, time_to_complite
 
 });
--- routes/order_route.js
+++ routes/order_route.js
@@ -8,13 +8,13 @@ module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
     if(req.session.name && req.session.id && req.session.phone_number){
-   
+
     //obtain data from database
      knex
       .select('restaurents.id','restaurents.name', 'menu_items.name','restaurent_id','price','menu_items.id','menu_items.description')
       .from("menu_items")
       .join('restaurents', {'restaurents.id':'menu_items.restaurent_id'})
-      .where('restaurent_id','=',req.params.id) 
+      .where('restaurent_id','=',req.params.id)
       .then((results) => {
         var templateVars  = {name:req.session.name, menus:results};
         res.render("order",templateVars);
@@ -29,21 +29,46 @@ module.exports = (knex) => {
 
   });
 
-  router.post("/:id", (req, res) => {
-    //to send message to customer that order is ready to pick up
-    var accountSid = 'AC324e4b947a9a446ad2250037f1e9f268'; // Your Account SID from www.twilio.com/console
-    var authToken = 'cc2f2227e77ac2b565ddab161df5b77c';   // Your Auth Token from www.twilio.com/console
+  router.post("/", (req, res) => {
+    if(req.session.name && req.session.phone_number && req.session.id){
 
 
-    var client = new twilio(accountSid, authToken);
 
-    client.messages.create({
-        body: 'Your order is ready',
-        to: '+16137203194',  // Text this number
-        from: '+12897961008' // From a valid Twilio number
-})
-.then((message) => console.log(message.sid));
-    res.redirect("/");
+
+      var accountSid = 'AC324e4b947a9a446ad2250037f1e9f268'; // Your Account SID from www.twilio.com/console
+      var authToken = 'cc2f2227e77ac2b565ddab161df5b77c';   // Your Auth Token from www.twilio.com/console
+
+      var client = new twilio(accountSid, authToken);
+
+      console.log('POST result is:' ,req.body);
+
+      //POST REQUEST FOR ORDER STATUS TABLE: id, customer_id, restaurant_id, items(object), total_price
+  // created_at, status, time_to_complite
+
+      knex('orders').insert([{
+                customer_id: req.session.id,
+                restaurent_id: parseInt(req.body.restaurant_id),
+                items: JSON.stringify(req.body.items),
+                total_price: parseInt(req.body.total_price)
+              }]).then(result => {
+                var accountSid = 'AC324e4b947a9a446ad2250037f1e9f268'; // Your Account SID from www.twilio.com/console
+                var authToken = 'cc2f2227e77ac2b565ddab161df5b77c';   // Your Auth Token from www.twilio.com/console
+                var client = new twilio(accountSid, authToken);
+
+                client.messages.create({
+                  body: 'Your order is ready',
+                  to: '+14384041793',  // Text this number
+                  from: '+12897961008' // From a valid Twilio number
+                })
+                .then((message) => console.log(message.sid));
+              })
+
+
+
+
+    }else{
+      res.send("please login")
+    }
   });
 
 
